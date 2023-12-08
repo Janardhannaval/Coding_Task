16a559925e51291803f28f55faa9f1d4e25a77b7
janardhan naval <jknaval564@gmail.com>
2023-12-08 11:59:21 +0530

    combine of all commit in one commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ import Home from "./pages/Home";
  
 function App() {
   
+
   return (
-   <Home/>
+    <>
+    {/* <Home/> */}
+   <h1> start test</h1>
+   <h1> start test1</h1>
+   <h1> start test3</h1>
+   <h1> start test4</h1>
+   <h1> resolve issues</h1>
+   <h1> resolve issues second</h1>
+    
+  </>
   )
 }
 export default App;
